a798b9fe3aae712a77f73e88cfc401a36cca3942
wangshangwen <shangwenwang20@gmail.com>
2025-03-28 23:49:49 +0800

    chore: init

--- examples/app.py
+++ examples/app.py
@@ -1,6 +1,7 @@
-from miu.core import encode_bytes
+from miu import base32_encode, base32_decode
 
+message = base32_encode(b"f", "Crockford")
 
-print(encode_bytes(b"f"))
+print(base32_decode(message, "Crockford").decode("utf-8"))
 
 
--- miu/core.py
+++ miu/core.py
@@ -1,8 +1,13 @@
-from base32_tiny import encode
+from base32_tiny import encode, decode
+from base32_tiny.encode import Variant, Options
 
 
-def encode_bytes(s: bytes) -> str:
-    return encode(s, variant="Crockford")
+def base32_encode(s: bytes, variants: Variant, option:  Options = None) -> str:
+    return encode(s, variant=variants, options=option)
+
+
+def base32_decode(s: str, variants: Variant) -> bytes:
+    return decode(s, variant=variants)
 
 
 
